da26818e001c3e643c57c7da73aebf51abdf90a4
Joseledgy <jrl22ledger@gmail.com>
2021-05-23 22:40:17 +0300

    Added the invoiceDelete function that will trigger if a invoice has been deleted, this will update the data in the tenant_invoice of its specific tenant. If there is existing data for the tenant, it will get the current_invoice data from the last invoice, add plus one in next_invoice, then get the updated length of the invoices.

--- functions/index.js
+++ functions/index.js
@@ -31,3 +31,35 @@ exports.onInvoiceCreate_1 = functions.firestore
                 num_invoices: increment
             });
     });
+
+exports.onInvoiceDelete = functions.firestore
+    .document('/invoices/{invoiceId}')
+    .onDelete(async (snapshot, context) => {
+        const invoiceDetails = snapshot.data();
+        let { tenant } = invoiceDetails;
+
+        const tenantid = tenant.split('/')[1];
+        const ref = admin.firestore().collection('tenant_invoices').doc(tenantid);
+
+        let invoicesSnap = await admin.firestore().collection('invoices').where('tenant', '==', tenant).orderBy('invoice_code', 'desc').limit(1).get();
+
+        if (!invoicesSnap.empty) {
+            let invoices = invoicesSnap.docs;
+            let { invoice_code } = invoices[0].data();
+
+            return ref
+                .update({
+                    current_invoice: invoice_code,
+                    next_invoice: invoice_code + 1,
+                    num_invoices: invoices.length
+                });
+        }
+        else {
+            return ref
+                .update({
+                    current_invoice: 0,
+                    next_invoice: 1,
+                    num_invoices: 0
+                });
+        }
+    });
